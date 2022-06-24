f53f4e0ae2a9d367b066e14b6f2d201ee4d4f7d1
teren07 <100883582+teren07@users.noreply.github.com>
2022-06-23 23:20:26 -0400

    Update user.py

--- group_project/flask_app/models/user.py
+++ group_project/flask_app/models/user.py
@@ -16,7 +16,7 @@ class User:
         self.password=data["password"]
         self.created_at=data["created_at"]
         self.updated_at=data["updated_at"]
-        self.ride = []
+        self.trip = []
 
 
     @classmethod
