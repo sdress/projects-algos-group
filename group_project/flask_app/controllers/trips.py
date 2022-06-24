654f75f6692d14eaac634774432b58e525564cda
sdress <sarah.dresser1@gmail.com>
2022-06-24 01:35:38 -0700

    adding session

--- group_project/flask_app/controllers/trips.py
+++ group_project/flask_app/controllers/trips.py
@@ -1,18 +1,16 @@
 from calendar import c
 from flask_app import app
-from flask import render_template, redirect,session, request
+from flask import render_template, redirect,session, request, flash
 # from flask_app.models import trip, user
 from flask_app.models.trip import Trip
 from flask_app.models.user import User
 
 @app.route('/dashboard')
 def show_dashboard():
-        # TO-DO:
-        # check if user in session (logged in)
-        # if 'user_id' not in session:
-            # return redirect('/logout')
-
-    return render_template('dashboard.html', all_trips=Trip.get_all())
+    if 'user_id' not in session:
+        flash('You must be logged in to access site')
+        return redirect('/')
+    return render_template('dashboard.html', all_trips=Trip.get_all(), user_id=session['user_id'])
 
 @app.route('/trip/new')
 def render_trip_form():
