638c86aa878693d9a70f99640e170a5ac8a05f0e
teren07 <100883582+teren07@users.noreply.github.com>
2022-06-23 23:15:22 -0400

    Update users.py

--- group_project/flask_app/controllers/users.py
+++ group_project/flask_app/controllers/users.py
@@ -3,3 +3,8 @@ from flask_bcrypt import Bcrypt
 bcrypt = Bcrypt(app)
 from flask import render_template, request, redirect, session, flash
 from flask_app.models import trip, user
+
+
+@app.route("/")
+def index_route():
+    return render_template("index.html")
